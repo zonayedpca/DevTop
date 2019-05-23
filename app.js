df10dab350ff5371674441e08ed3fe47f1e4896c
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-05-23 21:17:34 +0600

    tray added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray } = require('electron');
 const path = require('path');
 
 function isDev() {
@@ -7,14 +7,36 @@ function isDev() {
 
 const currentClientDir = path.resolve('client');
 const WINDOW_URL = isDev() ? `http://localhost:3000` : 'currentClientDir/build/index.html';
+const iconName = process.platform === 'darwin' ? 'iconTemplate.png':'windows-icon.png';
+const iconPath = path.join(__dirname, `./src/assets/img/${iconName}`);
+
 let mainWindow;
+let tray;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
-    height: 400,
+    height: 420,
     width: 360,
     frame: false,
-    resizable: false
+    resizable: false,
+    show: false
   });
   mainWindow.loadURL(WINDOW_URL);
+  tray = new Tray(iconPath);
+  tray.on('click', (event, bounds) => {
+    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
+    const { x, y } = bounds;
+    const { height, width } = mainWindow.getBounds();
+    if(process.env === 'darwin') {
+      mainWindow.setBounds({
+        x: x - (width/2),
+        y: 0
+      });
+    } else {
+      mainWindow.setBounds({
+        x: x - (width/2),
+        y: y - height
+      });
+    }
+  })
 })
