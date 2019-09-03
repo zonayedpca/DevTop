f76887b6618375623a16799d51734df898e3e65f
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-03 14:10:38 +0530

    logger removed

--- src/module/AppUpdater.js
+++ src/module/AppUpdater.js
@@ -1,14 +1,7 @@
 const { autoUpdater } = require('electron-updater');
 
-const { isDev } = require('../utils');
-
 class AppUpdater {
     constructor() {
-        if(isDev) {
-            const log = require('electron-log');
-            log.transports.file.level = 'debug';
-            autoUpdater.logger = log;
-        }
         autoUpdater.checkForUpdatesAndNotify();
     }
 }
