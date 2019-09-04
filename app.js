a8c18c2a553db3b17a77c41d600f77766173f4cb
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-04 21:50:45 +0530

    Auto Launch Added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { setScreenSize } = require('./src/config');
 
 const MainWindow = require('./src/module/MainWindow');
 const ApplicationTray = require('./src/module/ApplicationTray');
+const AppAutoLaunch = require('./src/module/AppAutoLaunch');
 const AppUpdater = require('./src/module/AppUpdater');
 
 const { clipboard, code } = require('./src/service');
@@ -20,11 +21,13 @@ const iconPath = path.join(__dirname, `./src/assets/img/${iconName}`);
 
 let mainWindow;
 let tray;
+let appAutoLaunch;
 let updater;
 
 app.on('ready', () => {
     setScreenSize();
     updater = new AppUpdater();
+    appAutoLaunch = new AppAutoLaunch();
     mainWindow = new MainWindow(
         {
             height: 420,
@@ -40,7 +43,7 @@ app.on('ready', () => {
         },
         WINDOW_URL
     );
-    tray = new ApplicationTray(iconPath, mainWindow);
+    tray = new ApplicationTray(iconPath, mainWindow, appAutoLaunch);
     clipboard(mainWindow);
     code(mainWindow);
 });
--- src/module/ApplicationTray.js
+++ src/module/ApplicationTray.js
@@ -1,14 +1,15 @@
-const { app, Tray, Menu } = require('electron');
+const { app, Tray, Menu, MenuItem } = require('electron');
 
 const { getPosition } = require('../utils');
 
 class ApplicationTray extends Tray {
-    constructor(iconPath, mainWindow) {
+    constructor(iconPath, mainWindow, appAutoLaunch) {
         super(iconPath);
         this.mainWindow = mainWindow;
         this.on('click', this.onClick.bind(this));
         this.on('right-click', this.onRightClick.bind(this));
         this.setToolTip('DevTop');
+        this.appAutoLaunch = appAutoLaunch;
     }
 
     onClick() {
@@ -23,26 +24,43 @@ class ApplicationTray extends Tray {
     }
 
     onRightClick() {
-        const menuConfig = Menu.buildFromTemplate([
-            {
-                label: 'DevTop',
-            },
-            {
-                label: 'Check for Updates',
-            },
-            {
+        const menu = new Menu();
+        menu.append(
+            new MenuItem({
+                label: 'DevTop Essentials',
+            })
+        );
+        menu.append(new MenuItem({ type: 'separator' }));
+        menu.append(
+            new MenuItem({
                 label: 'Start on Startup',
-                type: 'radio',
-                checked: true,
-            },
-            {
+                type: 'checkbox',
+                click: () => {
+                    this.appAutoLaunch.toggleAutoLaunch();
+                },
+                checked: this.appAutoLaunch.status,
+            })
+        );
+        menu.append(
+            new MenuItem({
+                label: 'Check for Updates',
+                type: 'checkbox',
+                checked: this.status,
+                click: () => {
+                    this.status = !this.status;
+                },
+            })
+        );
+        menu.append(new MenuItem({ type: 'separator' }));
+        menu.append(
+            new MenuItem({
                 label: 'Quit',
                 click: () => {
                     app.quit();
                 },
-            },
-        ]);
-        this.popUpContextMenu(menuConfig);
+            })
+        );
+        this.popUpContextMenu(menu);
     }
 }
 
