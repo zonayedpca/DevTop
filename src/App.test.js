3994e2b209e38eb7944efb61d288aa6c9df4be6f
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-21 11:48:41 +0530

    test act added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { act } from "react-dom/test-utils";
 import App from "./App";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  act(() => {
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
