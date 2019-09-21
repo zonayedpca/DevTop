d9b4b64fd06dfb7cfaf8843cf1f533a2e1a8ec39
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-21 06:22:39 +0000

    Deploy zonayedpca/DevTop to github.com/zonayedpca/DevTop.git:gh-pages

---
+++ precache-manifest.a885f4cc3b3c948f570e8eca3cec5000.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c3c564569950f647552d29af291c5c",
+    "url": "/DevTop/index.html"
+  },
+  {
+    "revision": "e9ccc598b81379e0ff64",
+    "url": "/DevTop/static/css/main.f28beac1.chunk.css"
+  },
+  {
+    "revision": "eb5717f7570e273d471b",
+    "url": "/DevTop/static/js/2.c96a6a5f.chunk.js"
+  },
+  {
+    "revision": "e9ccc598b81379e0ff64",
+    "url": "/DevTop/static/js/main.fd5c8b61.chunk.js"
+  },
+  {
+    "revision": "ab2e794a96f280eb4c49",
+    "url": "/DevTop/static/js/runtime~main.d00f92c2.js"
+  },
+  {
+    "revision": "5300f1847ba54202a53d50c289e6f999",
+    "url": "/DevTop/static/media/bookmark.5300f184.png"
+  },
+  {
+    "revision": "a68f624b4d0dce371c4e0cef9728e4d9",
+    "url": "/DevTop/static/media/clipboard.a68f624b.png"
+  },
+  {
+    "revision": "13feb2f42ed8fe61f17dae6fb47e1308",
+    "url": "/DevTop/static/media/code.13feb2f4.png"
+  },
+  {
+    "revision": "5f171ec7bbbb043a83f3fa50908b41d3",
+    "url": "/DevTop/static/media/intro.5f171ec7.svg"
+  },
+  {
+    "revision": "5d006de25a2b5a0d839eb32fb7718224",
+    "url": "/DevTop/static/media/shortlink.5d006de2.png"
+  },
+  {
+    "revision": "94290784ce2f767dc242f7521aabf0d5",
+    "url": "/DevTop/static/media/todo.94290784.png"
+  }
+]);
