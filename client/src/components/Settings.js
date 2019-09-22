08d243731cea9822b89d4c8758feea02bfa96019
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-22 12:06:07 +0530

    settings height done

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -15,7 +15,7 @@ import './settings.css';
 
 const Settings = ({ verifyGithubToken, verifyBitlyToken, resetTokens }) => {
     return (
-        <div className="settings">
+        <div className="settings-area">
             <AppBar
                 position="static"
                 color="default"
@@ -34,29 +34,35 @@ const Settings = ({ verifyGithubToken, verifyBitlyToken, resetTokens }) => {
                     <Typography variant="h6">Settings</Typography>
                 </Toolbar>
             </AppBar>
-            <div className="content">
-                <TokenInput
-                    onSubmit={verifyGithubToken}
-                    name="GitHub"
-                    instruction="Generate a personal access token from GitHub. This access token should have the scope to 'Create Gists'"
-                />
-                <TokenInput
-                    onSubmit={verifyBitlyToken}
-                    name="Bitly"
-                    instruction="Genrate a Generic Access token from Bitly"
-                />
-                <div className="aciton">
-                    <button onClick={() => resetTokens()} className="btn-reset">
-                        Reset
-                    </button>
-                </div>
-                <div className="privacy">
-                    <p>
-                        <strong>
-                            We don't store any of these keys. Everything is done
-                            locally. Thus your information is kept safe.
-                        </strong>
-                    </p>
+            <div className="settings">
+                <div className="content">
+                    <TokenInput
+                        onSubmit={verifyGithubToken}
+                        name="GitHub"
+                        instruction="Generate a personal access token from GitHub. This access token should have the scope to 'Create Gists'"
+                    />
+                    <TokenInput
+                        onSubmit={verifyBitlyToken}
+                        name="Bitly"
+                        instruction="Genrate a Generic Access token from Bitly"
+                    />
+                    <div className="aciton">
+                        <button
+                            onClick={() => resetTokens()}
+                            className="btn-reset"
+                        >
+                            Reset
+                        </button>
+                    </div>
+                    <div className="privacy">
+                        <p>
+                            <strong>
+                                We don't store any of these keys. Everything is
+                                done locally. Thus your information is kept
+                                safe.
+                            </strong>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
