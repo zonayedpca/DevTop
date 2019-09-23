8a38f26f97481c82ea4c71465cc64104a6a6d7e9
zonayedpca <zonayedpca@yahoo.com>
2019-09-23 06:43:57 +0000

    linux position fixed

--- src/utils/getPosition.js
+++ src/utils/getPosition.js
@@ -5,22 +5,29 @@ module.exports = (tray, window) => {
     const { x, y, height, width } = tray.getBounds();
     const { height: winHeight, width: winWidth } = window.getBounds();
     let setX, setY;
-    if(y < (SIZE_THRESHOLD + (winHeight / 2))) {
-        // top
-        setX = x - (winWidth/2 - (width/2));
-        setY = y + height;
-    } else if(y > SIZE_THRESHOLD && y > (display.height - SIZE_THRESHOLD)) {
-        // bottom
-        setX = x - (winWidth/2 - (width/2));
-        setY = y - winHeight;
-    } else if(x < (SIZE_THRESHOLD + winWidth)) {
-        // left
-        setX = x + width;
-        setY = y - (winHeight - height);
+    if(process.platform !== 'linux') {
+	if(y < (SIZE_THRESHOLD + (winHeight / 2))) {
+		// top
+		setX = x - (winWidth/2 - (width/2));
+		setY = y + height;
+	    } else if(y > SIZE_THRESHOLD && y > (display.height - SIZE_THRESHOLD)) {
+		// bottom
+		setX = x - (winWidth/2 - (width/2));
+		setY = y - winHeight;
+	    } else if(x < (SIZE_THRESHOLD + winWidth)) {
+		// left
+        	setX = x + width;
+        	setY = y - (winHeight - height);
+    	} else {
+        	// right
+        	setX = x - winWidth;
+        	setY = y - (winHeight - height);
+    	}
     } else {
-        // right
-        setX = x - winWidth;
-        setY = y - (winHeight - height);
+        // for linux
+	setX= (display.height + winWidth/2);
+	setY = display.y;	
     }
+    
     return { setX, setY };
 }
